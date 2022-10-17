de3cda238b6e58e0bfd029b0d0ccf647a585c8a8
Ishwinder1078 <115969716+Ishwinder1078@users.noreply.github.com>
2022-10-17 11:55:21 +0530

    Add files via upload

---
+++ html project/js1.js
@@ -0,0 +1,9 @@
+<!Doctyle html>
+<html>
+<body>
+<h1>My javascript</h1>
+<button type="button" onclick="document.getElementById("demo").innerHTML=
+Date()">Click</button>
+<p id="demo"></p>
+</body>
+</html>
